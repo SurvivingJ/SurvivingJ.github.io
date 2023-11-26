dce267483c7b1a5cf432ebceba08b6862bf9f3f2
SurvivingJ <jamesccmcb@gmail.com>
2023-11-26 21:16:04 +1100

    Static version

---
+++ PHP_version/footer.php
@@ -0,0 +1,7 @@
+<h2>Contact</h2>
+    <p>Email: <a href="mailto:your.email@example.com">your.email@example.com</a></p>
+    <ul>
+      <!-- Add your social media links here -->
+      <li><a href="https://linkedin.com/in/james-cc-mcburnie" target="_blank">LinkedIn</a></li>
+      <li><a href="https://github.com/yourusername" target="_blank">GitHub</a></li>
+    </ul>
